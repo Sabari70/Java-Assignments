5db53b3f9aab3a7ebf772c7034b3b6009c8f8e5a
Sabari <37924763+Sabari70@users.noreply.github.com>
2018-08-08 18:40:28 +0530

    Update Java1.java

--- Java1.java
+++ Java1.java
@@ -2,19 +2,19 @@
 import java.util.*;
 import java.io.*;
 
-class Bank
+class Bank		//Polymorphism
 {  
     int getRateOfInterest(){
         return 0; }  
 }  
-  class ICICI extends Bank
+  class ICICI extends Bank		//Inheritance
   {  
     int getRateOfInterest(int x){
         return x*7; }  
   }  
 class AXIS extends Bank
 {
-    int getRateOfInterest(int x){
+    int getRateOfInterest(int x){	//Overriding
         return x*9; }  
 }
 class Overload
@@ -22,7 +22,7 @@ class Overload
     public int addition(int x, int y){
         return x+y;
     }
-    public int addition(int x, int y,int z){
+    public int addition(int x, int y,int z){		//Overloading
         return x+y+z;
     }
     
@@ -31,21 +31,22 @@ class Codechef
 {
 	public static void main (String[] args)
 	{
-	    ICICI i=new ICICI();  
+	ICICI i=new ICICI();  
         AXIS a=new AXIS();  
         Overload ov=new Overload();
         System.out.println("ICICI Rate of Interest: "+i.getRateOfInterest(5));  
         System.out.println("AXIS Rate of Interest: "+a.getRateOfInterest(9));
-		System.out.println("Sumof 2 numbers: "+ov.addition(5,10));
+		System.out.println("Sum of 2 numbers: "+ov.addition(5,10));
 		System.out.println("Sum of 3 numbers: "+ov.addition(5,10,15));
-		try {
-         int arr[] = new int[2];
-         System.out.println("Access element three :" + arr[3]);
-      } catch (ArrayIndexOutOfBoundsException e) {
-         System.out.println("Exception thrown  :" + e);
+		try {				//Exception Handling
+         		int arr[] = new int[2];
+        		System.out.println("Access element three :" + arr[3]);
+      		    } 
+		catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Exception thrown  :" + e);
+                }
+        System.out.println("Out of the block");
       }
-      System.out.println("Out of the block");
-   }
 		
 	
 }
