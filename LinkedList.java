bba10c775c3376730a96f35d71e794c744e4e9fc
Sabari <37924763+Sabari70@users.noreply.github.com>
2018-08-09 18:32:20 +0530

    Create LinkedList.java

---
+++ LinkedList.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args)
+  {
+	    List la = new LinkedList();
+      la.add(5);
+      la.add(10);
+      la.add(15);
+      la.add(20);
+      la.add(150);
+      la.add(15);
+      la.add("asdf");
+      System.out.println();
+      System.out.println(" LinkedList Elements before removal");
+      System.out.println("\t" + la);
+      
+	}
+}
