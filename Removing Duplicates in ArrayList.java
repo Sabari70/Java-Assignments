7331bc154a384fae3edc3cc14f6f24cf219bae4a
Sabari <37924763+Sabari70@users.noreply.github.com>
2018-08-10 10:16:04 +0530

    Create Removing Duplicates in ArrayList.java

---
+++ Removing Duplicates in ArrayList.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		ArrayList<integer> al = new ArrayList<>();
+        al.add(1);
+        al.add(1);
+        al.add(1);
+        al.add(1);
+        al.add(1);
+        al.add(1);
+        
+        System.out.println("Before removal: " + al);
+ 
+        for(int i = 0; i < al.size(); i++) {
+            for(int j = i + 1; j < al.size(); j++) {
+                if(al.get(i).equals(al.get(j))){
+                    al.remove(j);
+                    j--;
+                }
+            }
+        }
+ 
+        System.out.println("After removal: " + al);
+	}
+}
